c5271d1e1947997cc95915bfa1670a7ebd720f31
danielemarzullo96 <142089307+danielemarzullo96@users.noreply.github.com>
2023-09-01 18:33:43 +0200

    Create Dipendente.java

---
+++ Dipendente.java
@@ -0,0 +1,35 @@
+package com.mycompany.testsottoclasse;
+import java.util.Date;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+
+
+/*
+ *
+ * @author SERGIO MURASCO
+ */
+public class Dipendente extends Persona{
+    private int AnniLavoro;
+    private float Stipendio;
+    
+    public Dipendente(String _Nome, String _Cognome, Date _DataNascita, int _AnniLavoro, float _Stipendio)
+    {
+        super();
+        super.setNome(_Nome);
+        super.setCognome(_Cognome);
+        super.setDataNascita(_DataNascita);
+        this.AnniLavoro=_AnniLavoro;
+        this.Stipendio=_Stipendio;
+        
+       
+    }
+   public String toString()
+   {
+     String  tmp=super.toString();
+     tmp=tmp+"\n AnniDiLavoro"+AnniLavoro;
+     tmp=tmp+"\n Stipendio"+Stipendio;
+     return tmp;
+   }
+}
